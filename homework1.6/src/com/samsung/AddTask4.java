1f593dc51cb84cd2ffb2ac9bf93e6d313aef3825
maryshirk <b06050382@gmail.com>
2022-10-11 16:18:28 +0300

    change

---
+++ homework1.6/src/com/samsung/AddTask4.java
@@ -0,0 +1,21 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class AddTask4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a);
+            } else System.out.println(c);
+        } else {
+            if (b > c) {
+                System.out.println(b);
+            } else System.out.println(c);
+        }
+    }
+}
