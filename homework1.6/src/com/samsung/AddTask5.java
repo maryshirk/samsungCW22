ffc333fa9ef3d6e8f465858e934e0b543e109528
maryshirk <b06050382@gmail.com>
2022-10-11 16:21:10 +0300

    change

---
+++ homework1.6/src/com/samsung/AddTask5.java
@@ -0,0 +1,30 @@
+package com.samsung;
+
+import java.util.Scanner;
+
+public class AddTask5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        if (a >= b && b >= c) {
+            System.out.println(c + " " + b + " " + a);
+        }
+        else if (a >= c && c >= b) {
+            System.out.println(b + " " + c + " " + a);
+        }
+        else if (b >= a && a >= c) {
+            System.out.println(c + " " + a + " " + b);
+        }
+        else if (b >= c && c >= a) {
+            System.out.println(a + " " + c + " " + b);
+        }
+        else if (c >= a && a >= b) {
+            System.out.println(b + " " + a + " " + c);
+        }
+        else if (c >= b && b >= a) {
+            System.out.println(a + " " + b + " " + c);
+        }
+    }
+}
