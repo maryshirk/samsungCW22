867c287ea6924b036d689a5aee602cf1777f6523
maryshirk <b06050382@gmail.com>
2023-02-09 17:16:32 +0300

    Update App.java

--- test070223/src/com/samsung/App.java
+++ test070223/src/com/samsung/App.java
@@ -2,6 +2,9 @@ package com.samsung;
 
 import com.samsung.lesson.MyArrayList;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class App {
     public static void main(String[] args) {
         MyArrayList arrayList = new MyArrayList();
@@ -41,5 +44,12 @@ public class App {
         for (Object o: arrayList) {
             System.out.println(o);
         }
+
+        /*Iterator iterator = arrayList.iterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }*/
+
+        
     }
 }
